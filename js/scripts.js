a0f912b95501f05a81653a334f3540bda53e4ac6
Jason Paris <paris3200@gmail.com>
2023-10-24 23:14:17 -0400

    Add feature to resize grid

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,55 @@
+function createBox() {
+  const div = document.createElement('div');
+  div.classList.add('square');
+  return div;
+}
 
-function drawRow(){
-  const row = document.createElement('div')
-  row.classList.add("row")
+function createRow(numSquares) {
+  const row = document.createElement('div');
+  row.classList.add('row');
 
-  for (let x = 0; x < 16; x++){
-    div = createBox()
-    row.appendChild(div)
+  for (let x = 0; x < numSquares; x += 1) {
+    const div = createBox();
+    row.appendChild(div);
 
-    div.addEventListener('mouseover', function (e){
-      e.target.style.background = "black";
-    })
+    div.addEventListener('mouseenter', (e) => {
+      e.target.style.background = 'black';
+    });
   }
-  const container = document.getElementById("container")
-  container.append(row)
+  const container = document.getElementById('grid-container');
+  container.append(row);
 }
 
-function createBox(){
-  const div = document.createElement('div')
-  div.classList.add("square")
-  return div
+function resizeBox(numSquares) {
+  const size = Math.floor((960 - ((numSquares * 2) - 2)) / numSquares);
+
+  const squares = document.querySelectorAll('.square');
+  for (let x = 0; x < squares.length; x += 1) {
+    squares[x].style.width = `${size}px`;
+    squares[x].style.height = `${size}px`;
+  }
 }
 
+function removeGrid() {
+  const squares = document.querySelectorAll('.square');
+  for (let x = 0; x < squares.length; x += 1) {
+    squares[x].remove();
+  }
+}
 
-for (let x = 0; x < 16; x++){
-  drawRow()
-};
+function createGrid(numSquares) {
+  for (let x = 0; x < numSquares; x += 1) {
+    createRow(numSquares);
+  }
+  resizeBox(numSquares);
+}
+
+createGrid(30);
+
+const button = document.getElementById('redraw');
+button.addEventListener('click', () => {
+  const slider = document.getElementById('slider');
+  const gridSize = slider.value;
+  removeGrid();
+  createGrid(gridSize);
+});
